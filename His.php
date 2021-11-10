ab58d994daec4663aaf3d9eca35d6d3ec6c90935
Nguyen-Khanh-An <91657471+Nguyen-Khanh-An@users.noreply.github.com>
2021-11-10 23:36:05 +0700

    AQT
    ALS

--- His.php
+++ His.php
@@ -238,27 +238,27 @@
                         <!--Load san pham tu DB -->
                            <?php
 						  // 	include_once("database.php");
-		  				   	$result = mysqli_query($conn, "SELECT * FROM product" );
+		  				   	$result = pg_query($conn, "SELECT * FROM product" );
 			
 			                if (!$result) { //add this check.
-                                die('Invalid query: ' . mysqli_error($conn));
+                                die('Invalid query: ' . pg_errormessage($conn));
                             }
 		
 			            
-			                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+			                while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)){
 				            ?>
 				            <!--Một sản phẩm -->
                             <div class="single-product">
                                 <div class="product-f-image">
-                                    <img src="product-imgs/<?php echo $row['Pro_image']?>" width="150" height="150">
+                                    <img src="product-imgs/<?php echo $row['pro_image']?>" width="150" height="150">
                                     
                                 </div>
                                 
-                                <h2><a href="?page=quanly_chitietsanpham&ma=<?php echo  $row['Product_ID']?>"><?php echo  $row['Product_Name']?></a></h2>
+                                <h2><a href="?page=quanly_chitietsanpham&ma=<?php echo  $row['product_id']?>"><?php echo  $row['product_name']?></a></h2>
                                 
                                 <div class="product-carousel-price">
-                                    <ins><?php echo  $row['Price']?></ins> <del><?php echo  $row['oldPrice']?></del>
-									<p class="card-text"><?php echo  $row['DetailDesc']?></p>
+                                    <ins><?php echo  $row['price']?></ins> <del><?php echo  $row['oldprice']?></del>
+									<p class="card-text"><?php echo  $row['detaildesc']?></p>
                                 </div> 
                             </div>
 
