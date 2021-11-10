3798cd3ab29ce9baa6ff2360cc5c31ed5a815c36
Nguyen-Khanh-An <91657471+Nguyen-Khanh-An@users.noreply.github.com>
2021-11-10 21:51:59 +0700

    AL

--- Connection.php
+++ Connection.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'online shopping')
+    $conn = mysqli_connect("
+    postgres://rrpqiprifkondy:84bf70d8cac18c8242f2c85e514742513e745028900cb927848041983738d6ca@ec2-3-229-166-245.compute-1.amazonaws.com:5432/d6l3pme0cvi7ek")
         or die("Cannot connect database".mysqli_connect_error());
 ?>
 
